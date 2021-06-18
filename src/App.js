e0ed0dea56c88d5ef83db98995758995e35bc26f
Harbas <rijad.harbas@accenture.com>
2021-06-18 11:12:17 +0200

    Added basic header layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react"
 import './App.css';
+import Header from './header/Header';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header />
       </div>
     );
   }
---
+++ src/header/Highlight.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+class Highlight extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Highlight</h1>
+            </div>
+        )
+    }
+}
+
+export default Highlight;
